f9edd759fd0051b1e5ebfd080429409100267d35
Fox Wilson <foxwilson123@gmail.com>
2015-01-16 22:02:25 -0500

    gitignore, app refactoring

---
+++ run.py
@@ -0,0 +1,2 @@
+from web import app
+app.run(debug=True, port=5000)
--- web/__init__.py
+++ web/__init__.py
@@ -6,7 +6,7 @@ app = Flask('web')
 def add_routes(rule_endpoint_tuples):
     for rule, endpoint in rule_endpoint_tuples:
         print(rule, endpoint)
-        app.add_url_rule(rule, endpoint, methods=["GET", "POST"])
+        app.add_url_rule(rule, view_func=endpoint, methods=["GET", "POST"])
 
 add_routes([
     ('/', core.index)
