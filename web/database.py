7c3383a7d24403eb8a209d019e6c286f312bbb7f
Fox Wilson <foxwilson123@gmail.com>
2015-01-16 23:01:56 -0500

    database

---
+++ web/database.py
@@ -0,0 +1,17 @@
+from peewee import *
+from . import config
+db = PostgresqlDatabase(host='104.131.66.35', user='pennappsxi', password=config.password)
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class User(BaseModel):
+    username = CharField()
+    password = CharField()
+
+class Text(BaseModel):
+    text = TextField()
+    time = IntegerField()
+    location = CharField()
+    user = ForeignKeyField(User, related_name='texts')
