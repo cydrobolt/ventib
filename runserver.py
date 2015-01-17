fe2cfe7c2a25cd4162a7d99c9f9e0df09879de51
Fox Wilson <foxwilson123@gmail.com>
2015-01-16 21:53:55 -0500

    rename stuff and refactor app

--- runserver.py
+++
@@ -1,6 +0,0 @@
-# Start Ventib Server
-
-from Ventib import app
-
-if __name__ == "__main__":
-	app.run(debug=True)
--- web/__init__.py
+++ web/__init__.py
@@ -1,12 +1,16 @@
-from views import *
+from web.views import *
 from flask import Flask
 
-app = Flask('Ventib')
+app = Flask('web')
 
 def add_routes(rule_endpoint_tuples):
     for rule, endpoint in rule_endpoint_tuples:
+        print(rule, endpoint)
         app.add_url_rule(rule, endpoint, methods=["GET", "POST"])
 
 add_routes([
     ('/', core.index)
 ])
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
