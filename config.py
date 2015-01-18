abca1a72babd15ee01f7b00b5383be7961a93941
Chaoyi Zha <summermontreal@gmail.com>
2015-01-18 17:00:59 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Configuration File
+
+host = "path.to.your.server.com" # PostgreSQL database host
+user = "user" # PostgreSQL database username
+password = "password" # PostgreSQL database password
+database = "ventib" # PostgreSQL database name
